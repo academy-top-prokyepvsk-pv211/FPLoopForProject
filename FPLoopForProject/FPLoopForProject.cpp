62ae2a316b6cb06f67f15aec2f4f104aee9e671c
maxefimov <efimov.academy.top@gmail.com>
2023-02-20 17:14:04 +0300

    Добавьте файлы проекта.

---
+++ FPLoopForProject/FPLoopForProject.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int n{ 50 };
+    
+    
+    /*int amount{};
+    int i{ 1 };
+    while (i <= n)
+    {
+        amount += i;
+        i++;
+    }*/
+
+    /*int amount{};
+    for (int i{1}; i <= n; i++)
+        amount += i;
+
+    cout << amount << "\n";
+
+    amount = 0;
+    for(int i = n; i > 0; i--)
+        amount += i;
+
+    cout << amount << "\n";*/
+
+    //for (int i{}; i <= n; i++)
+    //{
+    //    /*if (i == 28)
+    //        break;*/
+    //        //goto label1;
+    //        
+    //    if (i % 2)
+    //        continue;
+    //    cout << i << " ";
+    //}
+    //cout << "\n";
+
+    /*for (int i{}; i <= n; i++)
+        if(i % 2 == 0)
+            cout << i << " ";
+    cout << "\n";*/
+
+    //for (int i{}; i <= n; i+=2)
+    //    cout << i << " ";
+
+    //int rows{ 5 }, cols{ 8 };
+
+    //for (int i = 0; i < rows; i++)
+    //{
+    //    for (int j = 0; j < cols; j++)
+    //        cout << i << " " << j << "\n";
+    //        //cout << setw(2) << "*";
+    //    //cout << "\n";
+    //}
+
+    int size{ 21 };
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+            if (i == j)
+                cout << setw(2) << "*";
+            else
+                cout << setw(2) << " ";
+        cout << "\n";
+    }
+    
+}
